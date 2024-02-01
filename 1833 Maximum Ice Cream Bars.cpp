7da34520d7b354bfb3bb98021356407b5861c307
rastri-dey <rastri@umich.edu>
2024-01-31 16:05:15 -0800

    Added description

--- 1833 Maximum Ice Cream Bars.cpp
+++ 1833 Maximum Ice Cream Bars.cpp
@@ -1,4 +1,10 @@
-'''
+/*
+Source: https://leetcode.com/problems/maximum-ice-cream-bars/
+Date: 31 Jan 2024
+Author: Rastri Dey
+*/
+/***********************************************************************************************
+Problem:
 It is a sweltering summer day, and a boy wants to buy some ice cream bars.
 At the store, there are n ice cream bars. You are given an array costs of length n, where costs[i] is the price of the ith ice cream bar in coins. The boy initially has coins coins to spend, and he wants to buy as many ice cream bars as possible. 
 Note: The boy can buy the ice cream bars in any order.
@@ -27,7 +33,7 @@ costs.length == n
 1 <= n <= 105
 1 <= costs[i] <= 105
 1 <= coins <= 108
-'''
+****************************************************************************************************/
 
 // Approach: Counting Sort & Greedy Algorithm
 // Time: O(n+m): n is length of input array & m is maximum element
