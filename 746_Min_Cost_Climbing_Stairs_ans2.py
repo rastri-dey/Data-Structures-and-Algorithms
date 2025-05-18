d21ab259a450fac0a695a73a7d80b2f539427ec4
rastri-dey <rastri@umich.edu>
2025-05-18 13:37:42 -0700

    DP Bottom Up

--- 746_Min_Cost_Climbing_Stairs_ans2.py
+++ 746_Min_Cost_Climbing_Stairs_ans2.py
@@ -55,11 +55,11 @@ class Solution:
             dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2])
 
         return dp[N]
-'''       
+       
 # Approach: Dynamic Programming Bottom Up Optimized
 # Time: O(N)
 # Space: O(1), Constant Space
-'''   
+  
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
        
