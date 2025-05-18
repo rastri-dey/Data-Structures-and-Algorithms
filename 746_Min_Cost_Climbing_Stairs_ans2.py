eceace517e5793c2354bc09dd082cc97d23488dd
rastri-dey <rastri@umich.edu>
2025-05-18 13:34:29 -0700

    DP Top Down/ Bottom Up

---
+++ 746_Min_Cost_Climbing_Stairs_ans2.py
@@ -0,0 +1,73 @@
+'''
+Source: https://leetcode.com/problems/min-cost-climbing-stairs/
+Date: 18 May 2025
+Author: Rastri Dey 
+
+************************************************************************************
+
+You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+
+You can either start from the step with index 0, or the step with index 1.
+
+Return the minimum cost to reach the top of the floor.
+
+ 
+
+Example 1:
+
+Input: cost = [10,15,20]
+Output: 15
+Explanation: You will start at index 1.
+- Pay 15 and climb two steps to reach the top.
+The total cost is 15.
+Example 2:
+
+Input: cost = [1,100,1,1,1,100,1,1,100,1]
+Output: 6
+Explanation: You will start at index 0.
+- Pay 1 and climb two steps to reach index 2.
+- Pay 1 and climb two steps to reach index 4.
+- Pay 1 and climb two steps to reach index 6.
+- Pay 1 and climb one step to reach index 7.
+- Pay 1 and climb two steps to reach index 9.
+- Pay 1 and climb one step to reach the top.
+The total cost is 6.
+ 
+
+Constraints:
+
+2 <= cost.length <= 1000
+0 <= cost[i] <= 999
+
+*************************************************************************************
+
+# Approach: Dynamic Programming Bottom Up Tabulation
+# Time: O(N)
+# Space: O(N)
+
+'''
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        N = len(cost)
+        dp = [0]*(N+1)
+
+        for i in range(2, N+1):
+            dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2])
+
+        return dp[N]
+'''       
+# Approach: Dynamic Programming Bottom Up Optimized
+# Time: O(N)
+# Space: O(1), Constant Space
+'''   
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+       
+        prev, prev_minus1 = 0, 0
+
+        for i in range(2, len(cost)+1):
+            current = min(prev+cost[i-1], prev_minus1+cost[i-2])
+            prev_minus1 = prev
+            prev = current
+
+        return current
