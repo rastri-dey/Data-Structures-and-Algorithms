256456525bdd479bf1591f8bbf6cc6a9526f398d
rastri-dey <rastri@umich.edu>
2025-05-19 21:38:21 -0700

    Top Down & Bottom Up

--- 300_Longest_Increasing_Subsequence.py
+++ 300_Longest_Increasing_Subsequence.py
@@ -55,3 +55,20 @@ class Solution:
             return maxLen
         
         return max(dp(i) for i in range(len(nums)))
+    
+# Approach: Bottom Up
+# Time: O(N^2)
+# Space: O(N)
+    
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        N = len(nums)
+        dp = [1]*(N)
+
+        for i in range(N):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j]+1)
+        
+        return max(dp)
+    
