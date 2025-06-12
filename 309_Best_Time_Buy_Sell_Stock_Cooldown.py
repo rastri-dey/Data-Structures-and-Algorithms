15e98b87c7f8a31ce83ca7b2c5b25218c535ec32
rastri-dey <rastri@umich.edu>
2025-06-11 22:12:49 -0700

    Dynamic Programming

---
+++ 309_Best_Time_Buy_Sell_Stock_Cooldown.py
@@ -0,0 +1,56 @@
+'''
+Source: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/description/
+Date: 11 Jun 2025
+Author: Rastri Dey 
+
+************************************************************************************
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times) with the following restrictions:
+
+After you sell your stock, you cannot buy stock on the next day (i.e., cooldown one day).
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+Example 1:
+
+Input: prices = [1,2,3,0,2]
+Output: 3
+Explanation: transactions = [buy, sell, cooldown, buy, sell]
+Example 2:
+
+Input: prices = [1]
+Output: 0
+ 
+
+Constraints:
+
+1 <= prices.length <= 5000
+0 <= prices[i] <= 1000
+
+************************************************************************************
+
+# Approach: Top Down
+# Time: O(N)
+# Space: O(N)
+'''
+
+from functools import cache
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        N = len(prices)
+        @cache
+        def dp(i: int, hold: int) -> int:
+            if(i >= N):
+                return 0
+
+            if(hold==0):
+                # Buy or Skip
+                return max(-prices[i]+dp(i+1,1), dp(i+1,hold))
+            else:
+                # Sell & cooldown or Skip
+                return max(prices[i]+dp(i+2,0), dp(i+1,hold))
+
+        return dp(0,0)
+        
