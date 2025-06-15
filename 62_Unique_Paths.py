c165e236d9fa97e65b75b95d68a409f7119ddc12
rastri-dey <rastri@umich.edu>
2025-06-15 12:33:54 -0700

    Memoization, Tabulation

--- 62_Unique_Paths.py
+++ 62_Unique_Paths.py
@@ -56,3 +56,20 @@ class Solution:
             return ways
 
         return memo(m-1,n-1)
+    
+# Approach: Bottom Up Iterative (Shouldn't be called as DP)
+# Time: O(M*N)
+# Space: O(M*N)
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        memo = [[0]*n for _ in range(m)]
+        memo[0][0] = 1
+        for i in range(m):
+            for j in range(n):
+                if i>0:
+                    memo[i][j]+=memo[i-1][j]
+                if j>0:
+                    memo[i][j]+=memo[i][j-1]
+
+        return memo[m-1][n-1]
