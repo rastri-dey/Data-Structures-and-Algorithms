b900371215379f07296f597c9adde3d7be117ae5
rastri-dey <rastri@umich.edu>
2025-06-29 16:12:07 -0700

    Pointers

--- 88_Merge_Sorted_Array.py
+++ 88_Merge_Sorted_Array.py
@@ -63,9 +63,23 @@ class Solution:
                 nums1[p] = nums2[p2]
                 p2+=1
         
+# Approach: 2 pointers - From End
+# Time: O(N+M)
+# Space: O(1)
 
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
 
+        p2 = n-1
+        p1 = m-1
 
-
-
-        
+        for p in range(m+n-1,-1,-1):
+            if p1<0 or (p2>=0 and nums2[p2]>nums1[p1]):
+                nums1[p] = nums2[p2]
+                p2-=1
+            else:
+                nums1[p] = nums1[p1]
+                p1-=1             
