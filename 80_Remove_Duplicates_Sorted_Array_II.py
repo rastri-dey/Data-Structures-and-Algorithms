c43421947b23662ed7be24ae7eb9a66f6d8b0651
rastri-dey <rastri@umich.edu>
2025-08-12 05:56:42 -0700

    2 Pointers

--- 80_Remove_Duplicates_Sorted_Array_II.py
+++ 80_Remove_Duplicates_Sorted_Array_II.py
@@ -68,3 +68,25 @@ class Solution:
                 j+=1
 
         return j
+    
+# Approach: 2 Pointers (Just another way)
+# Time: O(N)
+# Space: O(1)
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 1
+        count = 1
+        n = len(nums)
+
+        for i in range(1,n):
+            if nums[i] == nums[i-1]:
+                count += 1
+                if count>2:
+                    continue
+            else:
+                count = 1
+            
+            nums[j] = nums[i]
+            j+=1
+
+        return j
